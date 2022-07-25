211e0baf48a3177c45dbff7dd9f95abacb341e42
Ricardo Moura <ricardomoura1979@gmail.com>
2022-07-25 20:13:44 +0000

    game specifications and rules

--- questions.rb
+++ questions.rb
@@ -16,10 +16,10 @@ class Question
     answer = $stdin.gets.chomp.to_i
 
     if answer == num1 + num2
-      puts "Correct!"
+      puts "Nice, the answer is 100% correct!"
       return true
     else 
-      puts "Incorrect!"
+      puts "Sorry for read this, but it is totally incorrect!"
       return false
     end
   end
